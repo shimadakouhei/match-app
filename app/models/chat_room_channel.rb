d847fc1f09639dbc5052de78768e8fd630df881a
shimadakouhei <shimapykkm@gmail.com>
2020-04-23 01:10:27 +0900

    削除

--- app/models/chat_room_channel.rb
+++
@@ -1,2 +0,0 @@
-class ChatRoomChannel < ApplicationRecord
-end
--- test/models/chat_room_channel_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class ChatRoomChannelTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
